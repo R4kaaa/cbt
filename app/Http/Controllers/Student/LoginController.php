7a7d54c6e5e8d88289c71e7fe8ddf0b556534ee7
2131730057@student.polinema.ac.id <2131730057@student.polinema.ac.id>
2025-10-07 11:06:36 +0700

    perbaikan

--- app/Http/Controllers/Student/LoginController.php
+++ app/Http/Controllers/Student/LoginController.php
@@ -21,7 +21,7 @@ class LoginController extends Controller
             'phone' => 'required',
             'password'  => 'required',
         ]);
-        // dd($request->all());
+        dd($request->phone);
         //cek phone dan password
         $student = Student::where([
             'phone'      => $request->phone,
